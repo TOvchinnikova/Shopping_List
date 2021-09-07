72516661ed66e95ebc7bb19ea1a11056e4744edc
TOvchinnikova <gibadullinatanya@gmail.com>
2021-09-07 22:26:26 +0500

    Added database AppDatabase.kt. Created Entity ShopItemDbModel.kt Created Dao ShopListDao.kt

---
+++ app/src/main/java/com/t_ovchinnikova/android/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,24 @@
+package com.t_ovchinnikova.android.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.t_ovchinnikova.android.shoppinglist.domain.ShopItem
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDbModel>>
+
+    @Insert(onConflict = REPLACE)
+    fun addShopItem(shopItem: ShopItemDbModel)
+
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItem(shopItemId: Int)
+
+    @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId: Int): ShopItemDbModel
+}
