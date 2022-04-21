4ce35ea21009b2c4847cb82ce00b60859704198d
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2022-04-21 08:33:13 +0500

    Added ShopListProvider

--- app/src/main/java/com/t_ovchinnikova/android/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/t_ovchinnikova/android/shoppinglist/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.t_ovchinnikova.android.shoppinglist.presentation
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -46,6 +47,14 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
                 launchFragment(ShopItemFragment.newInstanceAddItem())
             }
         }
+        contentResolver.query(
+            Uri.parse("content://com.t_ovchinnikova.android.shoppinglist/shop_items"),
+            null,
+            null,
+            null,
+            null,
+            null
+        )
     }
 
     private fun isOnePaneMode(): Boolean {
